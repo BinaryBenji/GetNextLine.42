8f5be729701545faf300a51eb2e260a0cc34ae95
Benjamin <ben639dx@gmail.com>
2017-02-07 16:13:26 +0100

    WOW

--- get_next_line.c
+++ get_next_line.c
@@ -12,89 +12,109 @@
 
 #include "get_next_line.h"
 
-/*char* 	copy_case_zero(char *bufr, char *reader)
+
+/*char 	*ft_end_copy(char *buf)
 {
+	int begin;
 	int i;
+	char *s;
+	int len;
+	char *buf;
 
 	i = 0;
-	while (bufr[i] != '\0')
-		i++;
-	if (!(bufr = (char *)malloc(ft_strlen(bufr) + i + 1)))
+	begin = where_to_stop(buf,'\n');
+	len = ft_strlen(buf) - begin;
+	if (!(s = (char *)malloc(len * sizeof(char))))
 		return (NULL);
-	while (reader[i])
+	while(buf[begin])
+	{	
+		s[i] = buf[begin];
+		begin++;
+		i++;
+	}
+	free(buf);
+	if (!(buf = (char *)malloc(len - begin + 1 * sizeof(char))))
+		return (NULL);
+	while()
 	{
-		bufr[ft_strlen(bufr) + 1] = reader[i];
-		i++;
+
+
 	}
-	bufr[ft_strlen(bufr) + 1] = '\0';
-	return (bufr);
-}
-
-char* 	copy_case_line(char *bufr, char *reader)
-{
-	int i;
-
-	i = 0;
-	while (bufr[i] != '\n')
-		i++;
-	if (!(bufr = (char *)malloc(ft_strlen(bufr) + i + 1)))
-		return (NULL);
-	while (reader[i] != '\n')
-	{	
-		bufr[ft_strlen(bufr) + 1] = reader[i];
-		i++;
-	}
-	bufr[ft_strlen(bufr) + 1] = '\0';
-	return (bufr);
-}
-
-char*	copy_case_normal(char *bufr, char *reader)
-{
-	int i;
-
-	i = 0;
-	if (!(bufr = (char *)malloc(ft_strlen(bufr) + ft_strlen(reader))))
-		return (NULL);
-	while (reader[i] != '\0')
-	{	
-		bufr[ft_strlen(bufr) + 1] = reader[i];
-		i++;
-	}
-	bufr[ft_strlen(bufr) + 1] = '\0';
-	return (bufr);
+	return (buf);
 }*/
+
+int		where_to_stop(char *str,char stop)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == stop)
+			return (i);
+		i++;
+	}
+	return (i);
+}  
+
+char 	*ft_strjoin_stop(char *s1, char *s2)
+{
+	char	*s;
+	int		i;
+	int		j;
+	int 	stopper;
+
+	if (!s1 || !s2)
+		return (NULL);
+	stopper = where_to_stop(s2,'\n');
+	if (!(s = ft_strnew(ft_strlen(s1) + j + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	j = i;
+	i = 0;
+	while ((s2[i] != '\0') && (i < stopper))
+	{
+		s[j] = s2[i];
+		j++;
+		i++;
+	}  
+	s[j] = '\0';
+	return (s);
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	bufr[BUFF_SIZE + 1]; // Whole text before the last \n of the last exec
+	static char	bufr[BUFF_SIZE + 1]; // RIP
 	char 		*buf; // Buffer
 	int 		ret; // Stop
-	//char *reader;
+	int i;
 
-	while ((ret = read(fd, buf, BUFF_SIZE))) // While we are not at the EOF/\n.
+	i = 0;
+	//bufr doit stocker le reste
+	//line, la ligne
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		if (fd < 0 || !line || buf[0] == '\0') // Validity check
 			return (-1);
-		//if (!(ret = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
-		//	return (-2);
-		//ft_strcpy(reader,buf);
-		//printf("Le reader : %s \n", reader);
-		printf("Le buf : %s \n", buf);
-		if (ft_strchr(buf, '\0')) // Case Zero.
-		{	
-			printf("Including zero ");
-			//bufr = copy_case_zero(bufr, reader);
-			//return (0);
+		if (where_to_stop(buf, '\n') != ft_strlen(buf)) // Case line.
+		{
+			line = ft_strjoin_stop(line, buf);
+			printf("\n Baque dans le buf \n");
+			printf("Ligne : %s", line);
+			//free(buf);
+			//bufr = ft_end_copy(buf);
+			return (1);
 		}
-		if (ft_strchr(buf, '\n')) // Case line.
-		{	
-			printf("Including baque");
-			//bufr = copy_case_line(bufr, reader);
-			//return (1);
+		else // Case no line
+		{
+			line = ft_strjoin(line,buf);
+			printf("Pas encore a la fin de la ligne \n");
 		}
-		else //Case nothing.
-			//bufr = copy_case_normal(bufr, reader);
-			printf("noting");
 	}
-	return (1);	
+	return (0);	
 }

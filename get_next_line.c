5bc6d79de80f8a1361d68b45bc03e3ac2df75fb5
Benjamin <ben639dx@gmail.com>
2017-02-23 13:33:02 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,36 @@
 /*   By: bzmuda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 15:23:25 by bzmuda            #+#    #+#             */
-/*   Updated: 2017/01/17 15:24:10 by bzmuda           ###   ########.fr       */
+/*   Updated: 2017/02/23 11:24:43 by bzmuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "includes/get_next_line.h"
 
-char 	*readone(char *bufr, int fd)
+char	*readone(char *bufr, int fd)
 {
 	char shot[BUFF_SIZE + 1];
 
 	ft_strclr(bufr);
-	read(fd, shot, BUFF_SIZE);
+	if(read(fd, shot, BUFF_SIZE) == -1)
+		return(NULL);
 	shot[BUFF_SIZE] = '\0';
 	ft_strcpy(bufr, shot);
 	return (bufr);
-}	
+}
 
-char 	*cut_end(char *bufr)
+char	*cut_end(char *bufr)
 {
-	int i;
-	char *tosave;
+	int		i;
+	char	*tosave;
 
 	i = 0;
-	while(bufr[i] != '\n')
+	while ((bufr[i]) && (bufr[i] != '\n'))
 		i++;
-	if(!(tosave = (char *)malloc((i + 1)* sizeof(char))))
-		return NULL;
+	if (!(tosave = (char *)malloc((i + 1) * sizeof(char))))
+		return (NULL);
 	i = 0;
-	while(bufr[i] != '\n')
+	while ((bufr[i]) && (bufr[i] != '\n'))
 	{
 		tosave[i] = bufr[i];
 		i++;
@@ -43,36 +44,50 @@ char 	*cut_end(char *bufr)
 	return (tosave);
 }
 
-char 	*after_line(char *bufr)
+char	*after_line(char *bufr)
 {
-	int i;
-	int j;
-	char *str;
+	int		i;
+	int		j;
+	char	*str;
 
 	i = 0;
 	j = 0;
-	while(bufr[i] != '\n')
+	while (bufr[i] != '\n')
 		i++;
-	//ft_strclr(bufr);
-	if(!(str = (char *)malloc(BUFF_SIZE - i + 1 * sizeof(char))))
-		return NULL;
+	if (!(str = (char *)malloc(BUFF_SIZE - i + 1 * sizeof(char))))
+		return (NULL);
 	i++;
-	while(i < BUFF_SIZE)
+	while (i < BUFF_SIZE)
 	{
 		str[j] = bufr[i];
 		j++;
 		i++;
 	}
 	str[j] = '\0';
-	return(str);
+	return (str);
+}
+
+int 	check_end(char *bufr)
+{
+	int i;
+
+	i = 0;
+	while(i < BUFF_SIZE)
+	{
+		if(bufr[i] == '\0')
+			return (1);
+		i++;
+	}
+	return(0);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static char	*bufr;
-	char 		*save; // Saver --> will be pasted in line
+	char		*save;
 	char		*end;
 
+	save = NULL;
 	if (!line || fd < 0)
 		return (-1);
 	while (1)
@@ -82,19 +97,22 @@ int		get_next_line(const int fd, char **line)
 		if (!save)
 			save = ft_memalloc(1);
 		save = ft_strjoin(save, bufr);
-		if ((ft_strchr(bufr, '\n') == NULL) && (bufr[0] != '\0')) // Case no line.
+		if ((ft_strchr(bufr, '\n') == NULL) && (bufr[0] != '\0'))
 		{
 			*line = ft_strdup(save);
 			ft_strclr(bufr);
 		}
-		if (!(bufr == readone(bufr, fd)))
+		if (bufr != readone(bufr, fd))
 			return (-1);
-		if (ft_strchr(bufr, '\n') != NULL) // Break if line
-			break;
+		if (ft_strchr(bufr, '\n') != NULL || check_end(bufr) == 1)
+			break ;
 	}
 	end = cut_end(bufr);
-	bufr = after_line(bufr);
+	if (ft_strchr(bufr, '\n'))
+		bufr = after_line(bufr);
 	save = ft_strjoin(save, end);
 	*line = ft_strdup(save);
-	return (0);	
+	if (check_end(bufr) != 1)
+		return (1);
+	return (0);
 }
--- main.c
+++ main.c
@@ -5,8 +5,12 @@
 #include "get_next_line.h"
 int  main(int argc, char **argv)
 {
+
+  argc = 2;
   char  *line = NULL;
   int   fd = open(argv[1], O_RDONLY);
+  get_next_line(fd, &line);
+  printf("%s\n", line);
   get_next_line(fd, &line);
   printf("%s\n", line);
   get_next_line(fd, &line);

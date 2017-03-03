6e6d3fd858c783b9e6c5422029434fa51ff03385
Benjamin <ben639dx@gmail.com>
2017-03-03 22:29:17 +0100

    Clean main.c

--- main.c
+++ main.c
@@ -1,38 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include "get_next_line.h"
+
 int  main(int argc, char **argv)
 {
-
-  argc = 2;
   char  *line = NULL;
-  int ret;
+  int   ret;
   int   fd = open(argv[1], O_RDONLY);
-  
-  ret = get_next_line(fd, &line);
-  printf("ligne : %s#\n", line);
-  printf("return : %d\n", ret);
 
-  ret = get_next_line(fd, &line);
-  printf("ligne : %s#\n", line);
-  printf("return : %d\n", ret);
+  argc++; // Used to ignore -Werror
 
-  ret = get_next_line(fd, &line);
-  printf("ligne : %s#\n", line);
-  printf("return : %d\n", ret);
-
-  ret = get_next_line(fd, &line);
-  printf("ligne : %s#\n", line);
-  printf("return : %d\n", ret);
-
-  ret = get_next_line(fd, &line);
-  printf("ligne : %s#\n", line);
-  printf("return : %d\n", ret);
-  //close(fd);
-  //sleep(15);
-  // mouli : 3 5 6 7
-  // basic : 
+  while((ret = get_next_line(fd, &line) == 1))
+  {
+    printf("Lign : %s#\n", line);
+    printf("Return value : %d\n\n", ret);
+  }
   return (0);
 }
